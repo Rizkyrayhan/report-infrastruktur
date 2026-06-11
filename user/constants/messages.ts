77b9c2d06742c311ed563619494f7534bdb33a63
Revaliano Daffa Irawan <daffairawan212@gmail.com>
2026-06-11 02:07:30 -0500

    feat(user/constants): membuat standar pesan validasi dan sukses

---
+++ user/constants/messages.ts
@@ -0,0 +1,9 @@
+export const VALIDATION_MESSAGES = {
+  REQUIRED: 'Kolom ini wajib diisi',
+  MIN_LENGTH: (min: number) => `Minimal ${min} karakter`,
+  INVALID_NIK: 'NIK harus terdiri dari 16 digit angka',
+};
+
+export const SUCCESS_MESSAGES = {
+  REPORT_SUBMITTED: 'Laporan berhasil dikirim! Silakan simpan ID Laporan Anda untuk mengecek status.',
+};
