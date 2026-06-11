2ec27c2b0a7c59e6cd4911ac56f19f5e19a2f67b
Revaliano Daffa Irawan <daffairawan212@gmail.com>
2026-06-11 02:07:30 -0500

    feat(user/constants): menambahkan data dummy region Lampung

---
+++ user/constants/regions.ts
@@ -0,0 +1,10 @@
+export const DUMMY_REGIONS = [
+  'Bandar Lampung',
+  'Metro',
+  'Lampung Selatan',
+  'Lampung Tengah',
+  'Lampung Timur',
+  'Lampung Utara',
+  'Pesawaran',
+  'Pringsewu'
+];
